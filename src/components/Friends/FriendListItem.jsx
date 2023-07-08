d6b336de94d6d20d06f9fc4dfb42d81c5589736b
=Andrei <balasca.andrei@gmail.com>
2023-07-08 19:35:00 +0300

    resilved IsOnline

--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types';
 export const FriendListItem = props => {
   return (
     <div>
-      <span isOnline={props.isOnline}></span>
+      <div
+        className="status"
+        style={{ backgroundColor: props.isOnline ? 'green' : 'red' }}
+      ></div>
       <img src={props.avatar} alt="User avatar" width="48" />
       <p>{props.name}</p>
     </div>
